718034f1bb85fd01254e66176b0a882d110602c8
gdevops1 <aria.soft67@gmail.com>
2021-08-22 15:35:47 +0430

    my first com

---
+++ articles/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from . import models
+
+def articles_list(request):
+    articles = models.Article.objects.all().order_by('date')
+    args = {'articles':articles}
+    return render(request,'articles/articles_list.html', args)
+    # djangoblog/articles/templates/articles/articles_list.html
